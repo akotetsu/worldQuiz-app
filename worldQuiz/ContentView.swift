255953cfcc6e6ce5df50a1b13f72b1ba2c978559
akotetsu <yuanliqu@gmail.com>
2025-02-22 13:53:42 +0900

    Initial Commit

---
+++ worldQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  worldQuiz
+//
+//  Created by 原里駆 on 2025/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
